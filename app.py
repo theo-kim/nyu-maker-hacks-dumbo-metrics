35fd5ce092d55ded97df06a05ff5ed5cf33f7502
Theodore Kim <tk1931@nyu.edu>
2019-03-10 03:04:23 -0400

    Web Server HTML

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/dev/metrics', methods = ['POST', 'GET'])
+def route():
+    if (request.method == 'POST') :
+        print(request.get_json())
+    else :
+        print(request.args)
+    return "success" 
+
+@app.route('/', methods = ['POST', 'GET'])
+def hello():
+    return '''
+    <html>
+        <head>
+            <title>Test</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+    </html>
+'''
